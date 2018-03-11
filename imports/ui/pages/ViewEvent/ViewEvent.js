801cf946beaf2cb7d974f077f561f54c0a9da59c
Rob Meronek <rob@theboardr.com>
2018-03-11 15:09:40 -0400

    added swipes and ability to react to them

--- imports/ui/pages/ViewEvent/ViewEvent.js
+++ imports/ui/pages/ViewEvent/ViewEvent.js
@@ -4,6 +4,7 @@ import { ButtonToolbar, ButtonGroup, Button } from 'react-bootstrap';
 import { withTracker } from 'meteor/react-meteor-data';
 import { Meteor } from 'meteor/meteor';
 import { Bert } from 'meteor/themeteorchef:bert';
+import Swipeable from 'react-swipeable';
 import Events from '../../../api/Events/Events';
 import NotFound from '../NotFound/NotFound';
 import Loading from '../../components/Loading/Loading';
@@ -21,21 +22,44 @@ const handleRemove = (eventId, history) => {
   }
 };
 
+const swiping = (e, deltaX, deltaY, absX, absY, velocity) => {
+  console.log("You're Swiping...", e, deltaX, deltaY, absX, absY, velocity)
+}
+
+const swipingLeft = (e, absX) => {
+  console.log("You're Swiping to the Left...", e, absX)
+}
+
+const swiped = (e, deltaX, deltaY, isFlick, velocity) => {
+  console.log("You Swiped...", e, deltaX, deltaY, isFlick, velocity)
+}
+
+const swipedUp = (e, deltaY, isFlick) => {
+  console.log("You Swiped Up...", e, deltaY, isFlick)
+}
+
 const renderEvent = (event, match, history) => (event ? (
-  <div className="ViewEvent">
-    <div className="page-header clearfix">
-      <h4 className="pull-left">{ event && event.title }</h4>
-      <ButtonToolbar className="pull-right">
-        <ButtonGroup bsSize="small">
-          <Button onClick={() => history.push(`${match.url}/edit`)}>Edit</Button>
-          <Button onClick={() => handleRemove(event._id, history)} className="text-danger">
-            Delete
-          </Button>
-        </ButtonGroup>
-      </ButtonToolbar>
+  <Swipeable
+    onSwiping={swiping}
+    onSwipingLeft={swipingLeft}
+    onSwiped={swiped}
+    onSwipedUp={swipedUp}
+  >
+    <div className="ViewEvent">
+      <div className="page-header clearfix">
+        <h4 className="pull-left">{ event && event.title }</h4>
+        <ButtonToolbar className="pull-right">
+          <ButtonGroup bsSize="small">
+            <Button onClick={() => history.push(`${match.url}/edit`)}>Edit</Button>
+            <Button onClick={() => handleRemove(event._id, history)} className="text-danger">
+              Delete
+            </Button>
+          </ButtonGroup>
+        </ButtonToolbar>
+      </div>
+      { event && event.title }
     </div>
-    { event && event.title }
-  </div>
+  </Swipeable>
 ) : <NotFound />);
 
 const ViewEvent = ({
