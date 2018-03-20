3675a7d1d03758dd64bec7cac4ef17390d52405f
Rob Meronek <rob@meronek.com>
2018-03-20 12:40:22 -0400

    got occurrence options listing on new event occurrence logger.

--- imports/ui/components/EventOccurrenceEditor/EventOccurrenceEditor.js
+++ imports/ui/components/EventOccurrenceEditor/EventOccurrenceEditor.js
@@ -2,11 +2,13 @@
 
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Row, Col, Button } from 'react-bootstrap';
+import { Row, Col, Button, Checkbox, ListGroup, ListGroupItem } from 'react-bootstrap';
 import { Meteor } from 'meteor/meteor';
 import { Bert } from 'meteor/themeteorchef:bert';
+import { withTracker } from 'meteor/react-meteor-data';
 import Swipeable from 'react-swipeable';
 import validate from '../../../modules/validate';
+import Events from '../../../api/Events/Events';
 
 
 class EventOccurrenceEditor extends React.Component {
@@ -28,9 +30,6 @@ class EventOccurrenceEditor extends React.Component {
   }
   swipedRight() {
     console.log('You swiped to the the Right...move them to the first record, basically like next when you are at the end of the record list');
-  }
-  swipedUp() {
-    console.log('You swiped Up...move them to the new occurrence form.');
   }
 
   handleSubmit(form) {
@@ -61,36 +60,51 @@ class EventOccurrenceEditor extends React.Component {
   }
 
   render() {
-    const { occurrence } = this.props;
+    // const { occurenceOptions } = this.props.eventsOccurrenceOptions;
     return (
       <form ref={form => (this.form = form)} onSubmit={event => event.preventDefault()}>
+
+
+        <Row>
+          <Col xs={12} className="text-center">
+            <h4>Start Logging</h4>
+          </Col>
+          <Col xs={4} className="text-center">
+              Swipe Up:<br />New
+          </Col>
+          <Col xs={4} className="text-center">
+              Swipe Right:<br />View Last
+          </Col>
+          <Col xs={4} className="text-center">
+              Swipe Left:<br />View First
+          </Col>
+        </Row>
+
+
         <Swipeable
           onSwipedLeft={this.swipedLeft}
           onSwipedRight={this.swipedRight}
-          onSwipedUp={this.swipedUp}
         >
 
-          <Row>
-            <Col xs={12} className="text-center">
-              <h4>Start Logging</h4>
-            </Col>
-            <Col xs={4} className="text-center">
-              Swipe Up:<br />New
-            </Col>
-            <Col xs={4} className="text-center">
-              Swipe Right:<br />View Last
-            </Col>
-            <Col xs={4} className="text-center">
-              Swipe Left:<br />View First
-            </Col>
-            
-            
-          </Row>
+
+          {this.props.eventsOccurrenceOptions ?
+            <ListGroup>
+              {console.log('this.props.eventsOccurrenceOptions.occurrenceOptions is', this.props.eventsOccurrenceOptions.occurrenceOptions)}
+              {this.props.eventsOccurrenceOptions.occurrenceOptions.map((option) => {
+                const { title } = option;
+              return (
+                <ListGroupItem key={title}>
+                  <Checkbox defaultChecked={false}>{title}</Checkbox>
+                </ListGroupItem>
+              );
+              })}
+            </ListGroup>
+        : []}
+
         </Swipeable>
         here, build the list of occurrence options for this event and give them checkboxes next to each.
-        <Button type="submit" bsStyle="success">
-          {occurrence && occurrence._id ? 'Save Changes' : 'Add Occurrence'}
-        </Button>
+
+
       </form>
     );
   }
@@ -105,4 +119,18 @@ EventOccurrenceEditor.propTypes = {
   history: PropTypes.object.isRequired,
 };
 
-export default EventOccurrenceEditor;
+
+export default withTracker((_id) => {
+  // not sure how to get this other than from the url, this seems ghetto but fuck it
+  // console.log('event id from query string is', _id.history.location.pathname.split('/')[3]);
+  const eventId = _id.history.location.pathname.split('/')[3];
+  // console.log('eventId is ', eventId);
+  const subscription = Meteor.subscribe('event.view', eventId);
+
+  return {
+    loading: !subscription.ready(),
+    eventsOccurrenceOptions: Events.findOne(eventId),
+    eventId,
+  };
+})(EventOccurrenceEditor);
+
--- imports/ui/components/Navigation/Navigation.js
+++ imports/ui/components/Navigation/Navigation.js
@@ -11,7 +11,7 @@ const Navigation = props => (
   <Navbar>
     <Navbar.Header>
       <Navbar.Brand>
-        <Link to="/">Stattt</Link>
+        <Link to="/events">Stattt</Link>
       </Navbar.Brand>
       <Navbar.Toggle />
     </Navbar.Header>
